bf362a861d93a89c8019750c7eec11867d2f4a56
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-08-02 16:48:21 +0200

    Voeg `CalendarHelper` toe aan de container.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
 // This file gets included at the end of the application boot sequence
+
+use foulard\google\CalendarHelper;
+
+Locale::setDefault('nl');
+
+$container->registerSingleton([CalendarHelper::class, 'calendarHelper'], CalendarHelper::class);
