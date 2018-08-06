be26daa5385f003fa0dab8e7be6f9da9b8ae7b35
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-08-06 22:45:04 +0200

    Voeg id van echte FooBaragenda toe.

--- app/config/calendar.php
+++ app/config/calendar.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    'calendarID' => '4obbg2gu9nqhmf4ugnbe8vi9l4@group.calendar.google.com',
+    // Calendar ID's are no secret, the credentials to reach and edit them are.
+    'calendarID' => '1' === getenv('FOULARD_PROD')
+        ? 'q24tnl03hvka3hn0c8kcl8t3c0@group.calendar.google.com'
+        : '4obbg2gu9nqhmf4ugnbe8vi9l4@group.calendar.google.com',
 ];
