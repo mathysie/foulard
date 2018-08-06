9a06079ecd6992b465cc50e7c682d1d5d46dd73a
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-08-06 16:28:07 +0200

    Build parser for Google Calendar result

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,8 +2,10 @@
 
 // This file gets included at the end of the application boot sequence
 
+use foulard\calendar\CalendarParser;
 use foulard\google\CalendarHelper;
 
 Locale::setDefault('nl');
 
 $container->registerSingleton([CalendarHelper::class, 'calendarHelper'], CalendarHelper::class);
+$container->registerSingleton([CalendarParser::class, 'calendarParser'], CalendarParser::class);
--- foulard/calendar/CalendarParser.php
+++ foulard/calendar/CalendarParser.php
@@ -2,7 +2,74 @@
 
 namespace foulard\calendar;
 
+use foulard\calendar\events\AanvraagEvent;
+use foulard\calendar\events\AegirborrelEvent;
+use foulard\calendar\events\OverigEvent;
+use foulard\calendar\events\SchoonmaakEvent;
+use foulard\calendar\events\TappersbedankborrelEvent;
+use foulard\calendar\events\VergaderingEvent;
+use Google_Service_Calendar_Event;
+
 class CalendarParser
 {
+    protected $event_hints = [
+        'Ægirborrel',
+        'FooBarvergadering',
+        'S: ',
+        'Tappersbedankborrel',
+        'O: ',
+    ];
 
+    /**
+     * Parses an array of Google_Service_Calendar_Event
+     * to an array of Event.
+     *
+     * @param array $events An array of Google_Service_Calendar_Event
+     *
+     * @return array an array of Event
+     */
+    public function parseEvents(array $events): array
+    {
+        $res = [];
+
+        foreach ($events as $event) {
+            $res[] = $this->parseEvent($event);
+        }
+
+        return $res;
+    }
+
+    /**
+     * Parse Google_Service_Calendar_Event to Event.
+     *
+     * @param Google_Service_Calendar_Event $event
+     *
+     * @return Event
+     */
+    protected function parseEvent(Google_Service_Calendar_Event $event): Event
+    {
+        $pattern = '/(' . implode('|', $this->event_hints) . ')/A';
+        preg_match($pattern, $event->summary, $match);
+
+        if (empty($match)) {
+            return new AanvraagEvent($event);
+        }
+
+        switch ($match[1]) {
+            case 'Ægirborrel':
+                return new AegirborrelEvent($event);
+
+            case 'FooBarvergadering':
+                return new VergaderingEvent($event);
+
+            case 'O: ':
+                return new OverigEvent($event);
+
+            case 'S: ':
+                return new SchoonmaakEvent($event);
+
+            case 'Tappersbedankborrel':
+                return new TappersbedankborrelEvent($event);
+        }
+    }
 }
