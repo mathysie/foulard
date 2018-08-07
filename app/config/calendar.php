2e020895688a3b1daed9d158b0032f8450a8ccc8
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-08-07 13:46:33 +0200

    Change testing calendar

--- app/config/calendar.php
+++ app/config/calendar.php
@@ -4,5 +4,5 @@ return [
     // Calendar ID's are no secret, the credentials to reach and edit them are.
     'calendarID' => '1' === getenv('FOULARD_PROD')
         ? 'q24tnl03hvka3hn0c8kcl8t3c0@group.calendar.google.com'
-        : '4obbg2gu9nqhmf4ugnbe8vi9l4@group.calendar.google.com',
+        : 'p053d04c6d0hrdlrj86b86867k@group.calendar.google.com',
 ];
