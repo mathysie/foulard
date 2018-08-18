a69a7e7d0c9c01a8ab35d86c8dfdca2ec846b88b
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-08-18 20:08:34 +0200

    Refereer juiste klasse in bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 // This file gets included at the end of the application boot sequence
 
+use foulard\calendar\CalendarParser;
 use foulard\google\CalendarHelper;
-use foulard\google\CalendarParser;
 
 Locale::setDefault('nl');
 
