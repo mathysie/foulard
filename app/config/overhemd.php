ecacabc7821e06a8c9b9e281b5e091a3951f92f9
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-08-24 16:04:38 +0200

    Zet offset van maanden in einde in de config

--- app/config/overhemd.php
+++ app/config/overhemd.php
@@ -3,5 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'tapmail' => require 'overhemd/tapmail.php',
+    'tapmail'  => require 'overhemd/tapmail.php',
+    'calendar' => require 'overhemd/calendar.php',
 ];
---
+++ app/config/overhemd/calendar.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'defaults' => [
+        'einde' => '2 months',
+    ],
+];
