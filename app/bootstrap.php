51f07c101e0fb0518d9cf2ea0a674c6981dc677f
Mathijs Kolkhuis Tanke <mathijs@kolkhuistanke.nl>
2018-10-23 15:15:26 +0200

    Install Nuno
    Refs #2

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 // This file gets included at the end of the application boot sequence
 
+use mako\config\Config;
+use mako\syringe\Container;
+use nuno\NunoClient;
 use overhemd\calendar\CalendarParser;
 use overhemd\google\CalendarHelper;
 
@@ -11,3 +14,8 @@ Locale::setDefault('nl');
 
 $container->registerSingleton([CalendarParser::class, 'calendarParser'], CalendarParser::class);
 $container->registerSingleton([CalendarHelper::class, 'calendarHelper'], CalendarHelper::class);
+$container->registerSingleton([NunoClient::class, 'nuno'], function (Container $container) {
+    return $container->call(function (Config $config) {
+        return new NunoClient($config->get('nuno.key'), $config->get('nuno.url'));
+    });
+});
---
+++ app/config/nuno.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+ */
+
+return [
+    'key' => '28da3128-f8e3-4986-aa74-7d6d960668f0',
+    'url' => 'http://localhost:8001/api',
+];
